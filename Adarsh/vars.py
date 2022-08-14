4249fd5b49106a5be59569e0ca9b25b00e60eb53
Jeremiah Helfer <JHelfer@users.noreply.github.com>
2022-08-14 01:14:51 -0700

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 class Var(object):
 	MULTI_CLIENT = False
-	API_ID = int(getenv('18732376'))
+	API_ID = str(getenv('18732376'))
 	API_HASH = str(getenv('738479feb6d6215c1d3a64222daa7ba8'))
 	BOT_TOKEN = str(getenv('5397911007:AAGRNMnMh-Aa3DCcrm8D0UaZHxL-YSJNEH4'))
 	name = str(getenv('name', 'filetolinkbot'))
@@ -19,7 +19,7 @@ class Var(object):
 	PORT = int(getenv('PORT', 8080))
 	BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 	PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200")) # 20 minutes
-	OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
+	OWNER_ID = set(int(x) for x in os.environ.get("20838550747", "").split())
 	NO_PORT = bool(getenv('NO_PORT', False))
 	APP_NAME = None
 	OWNER_USERNAME = str(getenv('enjoy_jer'))
